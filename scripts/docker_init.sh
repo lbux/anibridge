a29c547bd820bff1ae27c21f21b2c99bea8b9e07
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-02-21 04:38:46 +0000

    fix: Handle possible event of group already existing in Docker container (alpine reserved UIDs)

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -3,17 +3,29 @@
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 UMASK=${UMASK:-022}
+APP_USER=abc
+APP_GROUP=abc
 
 log() {
     printf "%s - init - INFO\t%s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$1"
 }
 
-if ! getent group abc >/dev/null 2>&1; then
-    addgroup -g "$PGID" abc
+EXISTING_GROUP=$(getent group "$PGID" 2>/dev/null | cut -d: -f1)
+if [ -n "$EXISTING_GROUP" ]; then
+    APP_GROUP=$EXISTING_GROUP
+elif getent group "$APP_GROUP" >/dev/null 2>&1; then
+    groupmod -g "$PGID" "$APP_GROUP"
+else
+    addgroup -g "$PGID" "$APP_GROUP"
 fi
 
-if ! getent passwd abc >/dev/null 2>&1; then
-    adduser -u "$PUID" -G abc -s /bin/sh -D abc
+EXISTING_USER=$(getent passwd "$PUID" 2>/dev/null | cut -d: -f1)
+if [ -n "$EXISTING_USER" ]; then
+    APP_USER=$EXISTING_USER
+elif getent passwd "$APP_USER" >/dev/null 2>&1; then
+    usermod -u "$PUID" -g "$APP_GROUP" "$APP_USER"
+else
+    adduser -u "$PUID" -G "$APP_GROUP" -s /bin/sh -D "$APP_USER"
 fi
 
 chown -R "$PUID:$PGID" /app
@@ -31,4 +43,4 @@ fi
 CURRENT_UMASK=$(umask)
 log "Starting AniBridge (UID: $PUID, GID: $PGID, UMASK: $CURRENT_UMASK)"
 
-exec su-exec abc "$@"
+exec su-exec "$APP_USER" "$@"
