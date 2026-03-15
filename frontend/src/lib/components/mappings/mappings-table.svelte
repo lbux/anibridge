d5ee74bc6f252b4a7c33417907582b288573b51e
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-03-15 16:41:26 +0000

    stye: Format anilist enums on frontend

--- frontend/src/lib/components/mappings/mappings-table.svelte
+++ frontend/src/lib/components/mappings/mappings-table.svelte
@@ -169,12 +169,12 @@
                                                     {#if m.anilist.format}<span
                                                             class="truncate rounded bg-slate-800/70 px-1 py-0.5 tracking-wide uppercase"
                                                             title={m.anilist.format}
-                                                            >{m.anilist.format}</span>
+                                                            >{m.anilist.format.replaceAll('_', ' ')}</span>
                                                     {/if}
                                                     {#if m.anilist.status}<span
                                                             class="truncate rounded bg-slate-800/70 px-1 py-0.5 tracking-wide uppercase"
                                                             title={m.anilist.status}
-                                                            >{m.anilist.status}</span>
+                                                            >{m.anilist.status.replaceAll('_', ' ')}</span>
                                                     {/if}
                                                     {#if m.anilist.season && m.anilist.seasonYear}<span
                                                             class="truncate rounded bg-slate-800/70 px-1 py-0.5 tracking-wide uppercase"
